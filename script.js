e7bba26e1944ee80426908ecd4068c97c88958d6
AkhilReddyGundla <121927997+AkhilReddyGundla@users.noreply.github.com>
2023-09-08 17:05:16 +0530

    script.js
    updated all fratures

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let randomNumber = parseInt(Math.random()*100 + 1)   // random number is created
+let randomNumber = parseInt(Math.random()*100 + 1) 
 const inputNum = document.querySelector("#guess")
 const submit = document.querySelector("#submit-button")
 const prevGuessess = document.querySelector(".lastGuesses")
@@ -6,23 +6,17 @@ const remGuessess = document.querySelector(".remGuesses")
 const result = document.querySelector(".finalResult")
 const startOver = document.querySelector(".result")
 
-
-
 const p = document.createElement('p')
 
-let guessess = []   //storing previous gussess
+let guessess = []
 let playGame = true
-let guessessRem = 9  // total number of gussess allowed 9 to 0 
-//if gurrseeRem === -1 then we stop the game
-
-
-
-//if playGame is true
+let guessessRem = 10
 if(playGame){
     submit.addEventListener('click' , function(e){
-        e.preventDefault();  //default action that belongs to the event will not occur.
+        e.preventDefault();
         const userInput =parseInt(inputNum.value)
         inputNum.value= ''
+        
         validateInput(userInput);
     })
 }
@@ -35,7 +29,7 @@ function validateInput(userInput){
     }else if(userInput < 1){
         alert("enter number greater than 0")
     }else{
-        if(guessessRem < 1){
+        if(guessessRem == 1){
             displayGuess(userInput)
             displayMessage(`Game over , Better luck for next time`)
             displayMessage(`random number is ${randomNumber}`)
@@ -57,11 +51,12 @@ function checkGuess(userInput){
     }else{
         displayMessage("Number is TOOO High")
     }
-    displayGuess(userInput);
+    displayGuess(userInput)
 
 }
 function displayGuess(userInput){
-    guessessRem--; // decrementing gussess and updating
+    guessessRem--;
+    
     prevGuessess.innerHTML += ` ${userInput}`
     remGuessess.innerHTML = guessessRem
 }
@@ -81,13 +76,12 @@ function endGame(){
     p.style.width = "100%"
     p.style.cursor = "pointer"
     startOver.appendChild(p)
-    playGame = false; // stop new game
-    newGame(); //suggest user to start new game
+    playGame = false;
+    newGame()
 }
 function newGame(){
     const newBtn = document.createElement("button")
     p.addEventListener('click', function(e){
-        //Defining the project's standard operational characteristics
         randomNumber = parseInt(Math.random()*100 + 1)
         guessess = []
         guessessRem = 10
